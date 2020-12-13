3688111d6b496337b5057e4c217b516cd180609f
naoto911 <k_naot_318@yahoo.co.jp>
2020-12-13 17:39:59 +0900

    groups controller generate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   root 'groups#index'
 end
