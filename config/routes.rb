2b40fe1c3777e0a30c67f0d3c0ae56fb7f9f4323
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-03 22:26:03 +0900

    add resources :group

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/edit'
+  resources :groups, only: [:new, :show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'groups#index'
 end
