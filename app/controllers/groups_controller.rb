bc50075386306c1a104e25ac156a9fc24d39cb8e
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-04 11:13:52 +0900

    group new create initialize

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,8 +8,26 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    if group.save
+      flash[:notice] = "「#{group.name}」の掲示板を作成しました"
+      redirect_to groups_path
+    else
+      redirect_to new_group_path
+    end
   end
 
   def edit
   end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'groups#index'
-   resources :groups, only: [:new, :show, :edit, :index]
+   resources :groups, only: [:new, :create, :show, :edit, :index]
 end
