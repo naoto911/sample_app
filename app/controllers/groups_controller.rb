18225663935c43c807b49ce2378f82b939b0bc27
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-07 03:08:28 +0900

    _member view list changed

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_target_group, only: %i[show edit update destroy]
-  before_action :logged_in_user, only: %i[edit update destroy] #このアクションはログイン後しか実行できない
+  before_action :logged_in_user, only: %i[new edit update destroy] #このアクションはログイン後しか実行できない
   
   def index
     @groups = Group.all
