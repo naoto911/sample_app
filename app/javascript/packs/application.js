65ee257d7b44a8bc31fe286adf82b688e8791d84
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-06 16:19:39 +0900

    jquery and fontawasome instaledd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// 追記 21/1/6
-require('jquery')
+require('jquery') // 追記 21/1/6
 
 import 'bootstrap';
 import '../stylesheets/application';
+import "@fortawesome/fontawesome-free/js/all"; // 追記 21/1/6
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
-// 以下jquery導入のため追記 21/1/6
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
-)
-// ここまで
+// 追記 21/1/6
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
+// ここまで 21/1/6
 module.exports = environment
