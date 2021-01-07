a69e2d8db7be9d34b131d116d6951dfcc13cd16a
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-07 18:08:10 +0900

    miss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env == 'development'
-  (3..10).each do |i|
-    User.create(name: "ユーザー#{i}", email: "#{i}@#{i}",password_digest: "#{i}@#{i}")
-  end
+  #(3..10).each do |i|
+    #User.create(name: "ユーザー#{i}", email: "#{i}@#{i}",password_digest: "#{i}@#{i}")
+  #end
 end
