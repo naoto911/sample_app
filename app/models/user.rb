8663c048ec2e421d6b725f1b938900d25a922638
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-07 17:27:18 +0900

    users model validates add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  #ユーザー認証のpassword用の設定
   has_secure_password
+  #ユーザー認証登録時に必要な情報は未入力は不可
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password_digest
+  end
+  #emailは重複不可
   validates :email, uniqueness: true
 end
