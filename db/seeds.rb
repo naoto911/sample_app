a5b5d271c021f92cff85b0f3609c84e0baa82f22
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-07 17:49:35 +0900

    user sample data create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env == 'development'
-  #(1..5).each do |i|
-    #Group.create(name: "グループ#{i}", introduction: "私たちはグループ#{i}です")
-  #end
+  (3..10).each do |i|
+    User.create(name: "ユーザー#{i}", email: "#{i}@#{i}",password_digest: "#{i}@#{i}")
+  end
 end
