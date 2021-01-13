7321e4286891b2fe1a847fbd3c416197d27d19dc
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-14 00:20:33 +0900

    miss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery
-//= require rails-ujs
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 require('jquery') // 追記 21/1/6
-require('jquery_ujs') // 追記 21/1/6
+// require('jquery_ujs') // 追記 21/1/6
 
 import 'bootstrap';
-import '../stylesheets/application';
+import '../src/application.scss';
 import "@fortawesome/fontawesome-free/js/all"; // 追記 21/1/6
 
 // Uncomment to copy all static images under ../images to the output folder and reference
