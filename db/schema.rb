d79333366f15271e13fb2d33f992c66a9635301f
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-17 11:00:00 +0900

    rails g uploader & addImageToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_132510) do
+ActiveRecord::Schema.define(version: 2021_01_17_012333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_132510) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
