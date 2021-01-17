0ca44d065af174e9db5329caa7ef12e4f5b8e12e
naoto911 <k_naot_318@yahoo.co.jp>
2021-01-17 13:32:55 +0900

    profile edit image cache try & miss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmationm, :image)
+    params.require(:user).permit(:name, :email, :password, :password_confirmationm, :image, :image_cache)
   end
 
   def set_target_user
