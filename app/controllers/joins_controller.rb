7a1d7d77aebbef5242355a864e80c2074e27e8dc
naoto911 <k_naot_318@yahoo.co.jp>
2021-02-08 20:33:49 +0900

    group detaile link modify to switch user join level & _permit user group name

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -45,15 +45,16 @@ class JoinsController < ApplicationController
   #グループ参加申請処理
   def permit
     #グループに参加済みの人の処理
-    if @group.users.ids.include?(current_user.id) then
-      redirect_to group_path(@group), flash: { notice: "#{@group.name}にはすでに参加済みです"}
+    if @join.level == 1 then
+      redirect_back(fallback_location: root_path) #, flash: { notice: "#{@group.name}にすでに参加済みです"}
     #グループに新規参加の人の処理
     else 
-      @group.users << current_user #joinにidを格納させる
+      @join.level = 1 #joinのlevelを1(参加済)に変更　
+      @join.update
       Event.where(group_id: @group.id).each_with_index do |group_event| #Eventを次々に取得 1/12編集
         group_event.users << current_user #Anserに(event_id,user_id)を紐付ける 1/12追記
       end
-      redirect_to group_path(@group), flash: { notice: "「#{current_user.name}が#{@group.name}に参加しました"}
+      redirect_to group_path(@group), flash: { notice: "「#{User.find(@join.user_id).name}が#{@group.name}に参加しました"}
     end
   end 
 
