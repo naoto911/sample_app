7d666d5063c5f6c304f4f7cb4b330c45ecd0634a
naoto911 <k_naot_318@yahoo.co.jp>
2021-03-19 15:06:01 +0900

    gem-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = 'fog/aws' # 追加
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id), 
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id], 
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
-
-    config.fog_directory  = Rails.application.credentials.dig(:s3_fog_directory)
-    config.cache_storage = :fog
+  
+    config.fog_directory  = Rails.application.credentials[:aws][:s3_fog_directory]
   end
 end
