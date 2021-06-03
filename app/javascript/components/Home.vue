275389e1c9f28bb4b0737c94f859caea13aa456b
naoto911 <k_naot_318@yahoo.co.jp>
2021-06-03 18:35:58 +0900

    to use vuetify template

--- app/javascript/components/Home.vue
+++ app/javascript/components/Home.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <v-bottom-navigation v-model="value">
     <v-btn value="recent">
@@ -13,3 +14,62 @@
     </v-btn>
   </v-bottom-navigation>
 </template>
+-->
+
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      shrink-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Syumix</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="group in groups"
+            :key="group"
+            cols="4"
+          >
+            <v-card height="200">{{ group.name }}</v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// axiosを読み込む
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      //axiosで取得するデータを入れるため、空でOK
+      groups: []
+    }
+  },
+  //mountedでVueインスタンスのDOM作成完了直後に読み込む
+  mounted() {
+    axios
+      //GETリクエストでRailsで作成したjsonを取得する
+      .get('/api/v1/groups.json')
+      // .get('/api/v1/groups.json')
+      //response.data は RailsのUser.select(:id, :name, :email)
+      //これをdata()で定義したusersに代入する
+      .then(response => (this.groups = response.data))
+  }
+}
+
+</script>
