adbae8b70b4695f3cf118334630cff347798a07d
naoto911 <k_naot_318@yahoo.co.jp>
2021-06-03 19:57:08 +0900

    to use act-parameter for groups all & to use router-link for grouop#show

--- app/javascript/components/Group.vue
+++ app/javascript/components/Group.vue
@@ -1,23 +1,87 @@
 <template>
-  <table>
-    <tbody>
-      <tr>
-        <th>ID</th>
-        <th>名前</th>
-        <th>紹介</th>
-      </tr>
-      <tr v-for="group in groups" :key="group.id">
-        <td>{{ group.id }}</td>
-        <router-link
-          :to=" 'groups/' + (Number(group.id))"
-          active-class="link--active"
-          exact
-          class="link"
-        ><td>{{ group.name }}</td></router-link>
-        <td>{{ group.introduction }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      shrink-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Syumix</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="group in groups"
+            :key="group.id"
+            cols="4"
+          >
+          <!-- ここから -->
+          <router-link
+            :to=" 'groups/' + (Number(group.id))"
+            active-class="link--active"
+            exact
+            class="link"
+          >
+              <v-card
+                class="mx-auto"
+                max-width="344"
+              >
+                <v-img
+                  :src= "group.image.url"
+                  height="200px"
+                ></v-img>
+
+                <v-card-title>
+                  {{ group.name }}
+                </v-card-title>
+
+                <v-card-subtitle>
+                  {{ group.introduction }}
+                </v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn
+                    color="orange lighten-2"
+                    text
+                  >
+                    LEARN MORE
+                  </v-btn>
+
+                  <v-spacer></v-spacer>
+
+                  <v-btn
+                    icon
+                    @click="show = !show"
+                  >
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                  </v-btn>
+                </v-card-actions>
+
+                <v-expand-transition>
+                  <div v-show="show">
+                    <v-divider></v-divider>
+
+                    <v-card-text>
+                      ここに,グループ説明を表示
+                    </v-card-text>
+                  </div>
+                </v-expand-transition>
+              </v-card>
+            </router-link>
+            <!-- ここまで -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -27,6 +91,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      show: false,
       //axiosで取得するデータを入れるため、空でOK
       groups: []
     }
