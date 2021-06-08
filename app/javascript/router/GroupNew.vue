acebec9eca8735d3fa0aaed16b4e8ccdd65256d3
naoto911 <k_naot_318@yahoo.co.jp>
2021-06-08 16:36:48 +0900

    initial create form menu for GroupNew.vue

--- app/javascript/router/GroupNew.vue
+++ app/javascript/router/GroupNew.vue
@@ -1,9 +1,123 @@
 <template>
   <div>
-    <p>New</p>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+
+    <!-- <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox> -->
+
+    <!-- <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validate
+    </v-btn> -->
+
+    <v-btn
+      color="primary"
+      class="mr-4"
+      @click="reset"
+    >
+      Create
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+
+    <!-- <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn> -->
+  </v-form>
   </div>
 </template>
 
+<script>
+import axios from 'axios';
+
+  export default {
+    data: () => ({
+      valid: true,
+      group: [],
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  //mountedでVueインスタンスのDOM作成完了直後に読み込む
+    mounted() {
+      axios
+        .get('/api/v1/groups/new.json')
+        .then(response => (this.group = response.data))
+    }
+  }
+</script>
+
+<!--
 <script>
 // axiosを読み込む
 import axios from 'axios';
@@ -28,3 +142,4 @@ export default {
 
 <style scoped>
 </style>
+-->
