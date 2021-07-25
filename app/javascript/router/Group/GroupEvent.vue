a9329257043925b83ed353e9aa349962a0b2e813
naoto911 <k_naot_318@yahoo.co.jp>
2021-07-25 09:00:03 +0900

    appear group events on calendar but not use time

--- app/javascript/router/Group/GroupEvent.vue
+++ app/javascript/router/Group/GroupEvent.vue
@@ -231,8 +231,9 @@ export default {
     // names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
   }),
   mounted () {
-    this.$refs.calendar.checkChange();
     this.getEvent();
+    this.$refs.calendar.checkChange();
+    // this.getEvent();
   },
   methods: {
     viewDay ({ date }) {
@@ -274,24 +275,40 @@ export default {
       const min = new Date(`${start.date}T00:00:00`) //min = 開始日の00:00:00
       const max = new Date(`${end.date}T23:59:59`) //min = 終了日の00:00:00
       const days = (max.getTime() - min.getTime()) / 86400000 //max-min (秒→日換算)
-      const eventCount = this.rnd(days, days + 20) //表示期間(日)+20を最大としたランダム値を取得し,これをイベント生成個とする
+      //① const eventCount = this.rnd(days, days + 20) //表示期間(日)+20を最大としたランダム値を取得し,これをイベント生成個とする
+
+      const eventCount = this.events2.length //追加①
+
+      const modifyDate = (new Date('2000-01-01')).getTime()
+      console.log(modifyDate)
+
+      const timezoneDifference = 9*3600*100 //9時間をミリsecondに変換したもの
 
       //上で定まったイベント生成個分for文を回す
       //後で配列へ追加するための定数を毎loop算出
       for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
+        // const allDay = this.rnd(0, 3) === 0
+        // const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+        const dateTime = (new Date(this.events2[i].date)).getTime()
+        console.log(dateTime)
+        const firstTimestamp = (new Date(this.events2[i].starttime)).getTime()
+        // const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+        const first = new Date(firstTimestamp - modifyDate + dateTime)
+        console.log(first)
+        // const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+        const secondTimestamp = (new Date(this.events2[i].finishtime)).getTime()
+        //const second = new Date(first.getTime() + secondTimestamp)
+        const second = new Date(secondTimestamp - modifyDate + dateTime)
+        console.log(second)
 
       //上で求めた定数を用いて,配列にname,start,end,color,timedを追加
         events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],// namesからランダムに値を選定
+          // name: this.names[this.rnd(0, this.names.length - 1)],// namesからランダムに値を選定
+          name : this.events2[i].place,
           start: first, //表示期間中からランダムに日付+時間を選定 (イベント開始時刻)
           end: second, //表示期間中からランダムに日付+時間を選定 (イベント終了時刻)
           color: this.colors[this.rnd(0, this.colors.length - 1)], //colorsからランダムに値を選定
-          timed: !allDay, //0~3の乱数が0になったら(this.rnd(0, 3) === 0 ) true
+          //timed: !allDay, //0~3の乱数が0になったら(this.rnd(0, 3) === 0 ) true
         })
       }
 
