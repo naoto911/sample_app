f7085070416471ebba9aa9439b787bf48f0360c8
naoto911 <k_naot_318@yahoo.co.jp>
2021-09-02 13:01:41 +0900

    try and complete modal window for delte button on GroupDetail.vue

--- app/javascript/router/Group/GroupDetail.vue
+++ app/javascript/router/Group/GroupDetail.vue
@@ -8,9 +8,50 @@
       <v-col class="text-right">
         
         <!-- ①-1 ここから 削除ボタン -->
-          <v-btn @click="deleteGroup(group.id)" icon>
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn 
+                icon 
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                削除します。よろしいですか？
+              </v-card-title>
+
+              <v-card-text>
+                この操作は取り消せません。
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="dialog = false"
+                >
+                  キャンセル
+                </v-btn>
+                <v-btn
+                  color="error"
+                  text
+                  @click="deleteGroup(group.id)"
+                >
+                  削除
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         <!-- ①-1 ここまで 削除ボタン -->
 
         <!-- ①-2 ここから UserEditへのリンク -->
@@ -124,6 +165,7 @@ export default {
     return {
       length: 3,
       onboarding: 0,
+      dialog: false,
 
       group: [],
       users: [],
