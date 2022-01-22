2c2a01e2a4537ddb392cd8771fa5af1032abbdb1
naoto911 <k_naot_318@yahoo.co.jp>
2022-01-22 17:03:46 +0900

    try to use RESAS API but failure on BU_Header.vue

--- app/javascript/components/BU_Header.vue
+++ app/javascript/components/BU_Header.vue
@@ -54,3 +54,25 @@
 
   </v-card>
   <!-- ②ここまで 写真スライド -->
+
+
+<!-- ①-1 ここから 削除ボタン -->
+<v-btn icon>
+<!-- <v-btn @click="deleteUser(user.id)" icon> -->
+  <v-icon>mdi-delete</v-icon>
+</v-btn>
+<!-- ①-1 ここまで 削除ボタン -->
+
+
+<!-- ①-1 ここから 都道府県API -->
+    getArea(){
+      var area_url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+      axios
+      .get(area_url,{
+        headers: { "X-API-KEY": "imTqHdr2MusisKwbx1V4J3wS3XrVmwEb26Uv83qB" },
+      })
+      .then(response =>  {
+        this.area = response;
+      });
+    },
+<!-- ①-1 ここまで 都道府県API -->
